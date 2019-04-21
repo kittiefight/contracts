d05423c2412123649442d3e60da6218ecb3ca4d5
“wafflemakr” <wafflemakr@gmail.com>
2019-04-20 19:55:15 -0500

    added var and fee names contract

--- contracts/GameVarAndFee.sol
+++ contracts/GameVarAndFee.sol
@@ -26,6 +26,7 @@ import "./DSNote.sol";
 import "./modules/proxy/Proxied.sol";
 //import "./modules/databases/GameVarAndFeeDB.sol";
 import './modules/databases/GenericDB.sol';
+import './misc/VarAndFeeNames.sol';
 
 
 /**
@@ -37,10 +38,10 @@ import './modules/databases/GenericDB.sol';
  * current contract has 3.5k deployment cost. If needed, create a DB contract for 
  * setters and leave getters here.
  */
-contract GameVarAndFee is Proxied {
+contract GameVarAndFee is Proxied, VarAndFeeNames {
 
   //All keys in Generic DB will be hased with this constant
-  string internal constant TABLE_NAME = "GameVarAndFeeTable";
+  
 
   //GenericDB type variable to be used in the contract when storing
   GenericDB public genericDB;
@@ -62,152 +63,133 @@ contract GameVarAndFee is Proxied {
   /// @notice Sets the time in future that a game is to be played
   /// @dev check if only one setter function can be implemented
   function setVarAndFee(string calldata keyName, uint value) 
-  external onlyProxy{
+  external onlyProxy {
     bytes32 key = keccak256(abi.encodePacked(TABLE_NAME, keyName));
     genericDB.setUintStorage(CONTRACT_NAME_GAMEVARANDFEE, key, value);
   }
 
+  // ---------------------
 
-   // ----- GETTERS -------
+
+
+
+  // ----- GETTERS -------
     
   /// @notice Gets the time in future that a game is to be played
   function getFutureGameTime() 
-  public view returns(uint) {
-      bytes32 key = keccak256(abi.encodePacked(TABLE_NAME, "futureGameTime"));
-      return genericDB.getUintStorage(CONTRACT_NAME_GAMEVARANDFEE, key);
+  public view returns(uint) { 
+      return genericDB.getUintStorage(CONTRACT_NAME_GAMEVARANDFEE, FUTURE_GAME_TIME);
   }
   
   /// @notice Gets 2 min alloted time whereby both players must initiate start or forfeit game.
   function getGamePrestart() 
   public view returns(uint) {
-      bytes32 key = keccak256(abi.encodePacked(TABLE_NAME, "gamePrestart"));
-      return genericDB.getUintStorage(CONTRACT_NAME_GAMEVARANDFEE, key);
+      return genericDB.getUintStorage(CONTRACT_NAME_GAMEVARANDFEE, GAME_PRESTART);
   }
   
   /// @notice Gets Game duration, how long a game lasts
   function getGameDuration() 
   public view returns(uint) {
-      bytes32 key = keccak256(abi.encodePacked("gameDuration"));
-      return genericDB.getUintStorage(CONTRACT_NAME_GAMEVARANDFEE, key);
+      return genericDB.getUintStorage(CONTRACT_NAME_GAMEVARANDFEE, GAME_DURATION);
   }
   
   /// @notice Gets how long to wait for payment for kittie in kittiehell before kittie is lost forever 
   function getKittieHellExpiration() 
   public view returns(uint) {
-      bytes32 key = keccak256(abi.encodePacked("kittieHellExpiration"));
-      return genericDB.getUintStorage(CONTRACT_NAME_GAMEVARANDFEE, key);
+      return genericDB.getUintStorage(CONTRACT_NAME_GAMEVARANDFEE, KITTIE_HELL_EXPIRATION);
   }
   
   /// @notice Gets the time at which honey pot will be dissolved,after a game is over, used by time contract at end of gameduration
   function getHoneypotExpiration() 
   public view returns(uint) {
-      bytes32 key = keccak256(abi.encodePacked("honeypotExpiration"));
-      return genericDB.getUintStorage(TABLE_NAME, key);
+      return genericDB.getUintStorage(TABLE_NAME, HONEY_POT_EXPIRATION);
   }
   
   /// @notice Gets the farthest time in futre when a game can be schedule
   function getScheduleTimeLimits() 
   public view returns(uint) {
-      bytes32 key = keccak256(abi.encodePacked("scheduleTimeLimits"));
-      return genericDB.getUintStorage(CONTRACT_NAME_GAMEVARANDFEE, key);
+      return genericDB.getUintStorage(CONTRACT_NAME_GAMEVARANDFEE, SCHEDULE_TIME_LIMITS);
   }
   
   /// @notice Gets mount of initial KTY Tokens allowed to be drawn from EndowmentFund to be allocated to game
   function getTokensPerGame() 
   public view returns(uint) {
-      bytes32 key = keccak256(abi.encodePacked("tokensPerGame"));
-      return genericDB.getUintStorage(CONTRACT_NAME_GAMEVARANDFEE, key);
+      return genericDB.getUintStorage(CONTRACT_NAME_GAMEVARANDFEE, TOKENS_PER_GAME);
   }
   
   /// @notice Gets Amount of initial ETH allowed to be drawn from EndowmentFund to be allocated to game
   function getEthPerGame() 
   public view returns(uint) {
-      bytes32 key = keccak256(abi.encodePacked("ethPerGame"));
-      return genericDB.getUintStorage(CONTRACT_NAME_GAMEVARANDFEE, key);
+      return genericDB.getUintStorage(CONTRACT_NAME_GAMEVARANDFEE, ETH_PER_GAME);
   }
   
   /// @notice Gets Amount of games allowed per day 
   function getDailyGameAvailability() 
   public view returns(uint) {
-      bytes32 key = keccak256(abi.encodePacked("dailyGameAvailability"));
-      return genericDB.getUintStorage(CONTRACT_NAME_GAMEVARANDFEE, key);
+      return genericDB.getUintStorage(CONTRACT_NAME_GAMEVARANDFEE, DAILY_GAME_AVAILABILITY);
   }
   
   /// @notice Gets Times duration between each game per day
   function getGameTimes() 
   public view returns(uint) {
-      bytes32 key = keccak256(abi.encodePacked("gameTimes"));
-      return genericDB.getUintStorage(CONTRACT_NAME_GAMEVARANDFEE, key);
+      return genericDB.getUintStorage(CONTRACT_NAME_GAMEVARANDFEE, GAME_TIMES);
   }
   
   /// @notice Gets Games per day allowed per address "Games rate limit"
   function getGameLimit() 
   public view returns(uint) {
-      bytes32 key = keccak256(abi.encodePacked("gameLimit"));
-      return genericDB.getUintStorage(CONTRACT_NAME_GAMEVARANDFEE, key);
+      return genericDB.getUintStorage(CONTRACT_NAME_GAMEVARANDFEE, GAME_LIMIT);
   }
   
   /// @notice Gets fee paid to lift the limit of the number of games allowed per day, per address
   function getGamesRateLimitFee() 
   public view returns(uint) {
-      bytes32 key = keccak256(abi.encodePacked("gamesRateLimitFee"));
-      return genericDB.getUintStorage(CONTRACT_NAME_GAMEVARANDFEE, key);
+      return genericDB.getUintStorage(CONTRACT_NAME_GAMEVARANDFEE, GAMES_RATE_LIMIT_FEE);
   }
   
   /// @notice Gets Distribution Rates
   function getDistributionRates() 
   public view returns(uint[] memory rates) {
-      bytes32 key;
+            
+      rates[0] = genericDB.getUintStorage(CONTRACT_NAME_GAMEVARANDFEE, WINNING_KITTIE);
       
-      key = keccak256(abi.encodePacked("winningKittie"));
-      rates[0] = genericDB.getUintStorage(CONTRACT_NAME_GAMEVARANDFEE, key);
+      rates[1] = genericDB.getUintStorage(CONTRACT_NAME_GAMEVARANDFEE, TOP_BETTOR);
       
-      key = keccak256(abi.encodePacked("topBettor"));
-      rates[1] = genericDB.getUintStorage(CONTRACT_NAME_GAMEVARANDFEE, key);
+      rates[2] = genericDB.getUintStorage(CONTRACT_NAME_GAMEVARANDFEE, SECOND_RUNNER_UP);
       
-      key = keccak256(abi.encodePacked("secondRunnerUp"));
-      rates[2] = genericDB.getUintStorage(CONTRACT_NAME_GAMEVARANDFEE, key);
+      rates[3] = genericDB.getUintStorage(CONTRACT_NAME_GAMEVARANDFEE, OTHER_BETTORS);
       
-      key = keccak256(abi.encodePacked("otherBettors"));
-      rates[3] = genericDB.getUintStorage(CONTRACT_NAME_GAMEVARANDFEE, key);
-      
-      key = keccak256(abi.encodePacked("endownment"));
-      rates[4] = genericDB.getUintStorage(CONTRACT_NAME_GAMEVARANDFEE, key);
+      rates[4] = genericDB.getUintStorage(CONTRACT_NAME_GAMEVARANDFEE, ENDOWNMENT);
   }
 
   /// @notice Gets ticket fee in KTY for betting participators
   function getTicketFee() 
   public view returns(uint) {
-      bytes32 key = keccak256(abi.encodePacked("ticketFee"));
-      return genericDB.getUintStorage(CONTRACT_NAME_GAMEVARANDFEE, key);
+      return genericDB.getUintStorage(CONTRACT_NAME_GAMEVARANDFEE, TICKET_FEE);
   }
 
   /// @notice Gets betting fee in KTY for betting participators
   function getBettingFee() 
   public view returns(uint) {
-      bytes32 key = keccak256(abi.encodePacked("bettingFee"));
-      return genericDB.getUintStorage(CONTRACT_NAME_GAMEVARANDFEE, key);
+      return genericDB.getUintStorage(CONTRACT_NAME_GAMEVARANDFEE, BETTING_FEE);
   }
 
   /// @notice Gets kittieHELL redemption fee in KTY for redeeming kitties
   function getKittieRedemptionFee() 
   public view returns(uint) {
-      bytes32 key = keccak256(abi.encodePacked("kittieRedemptionFee"));
-      return genericDB.getUintStorage(CONTRACT_NAME_GAMEVARANDFEE, key);
+      return genericDB.getUintStorage(CONTRACT_NAME_GAMEVARANDFEE, KITTIE_REDEMPTION_FEE);
   }
 
   /// @notice Gets Kittie expiry time in kittieHELL 
   function getKittieExpiry() 
   public view returns(uint) {
-      bytes32 key = keccak256(abi.encodePacked("kittieExpiry"));
-      return genericDB.getUintStorage(CONTRACT_NAME_GAMEVARANDFEE, key);
+      return genericDB.getUintStorage(CONTRACT_NAME_GAMEVARANDFEE, KITTIE_EXPIRY);
   }
 
   /// @notice Gets honeyPot duration/expiry after game end 
   function getHoneyPotDuration() 
   public view returns(uint) {
-      bytes32 key = keccak256(abi.encodePacked("honeyPotDuration"));
-      return genericDB.getUintStorage(CONTRACT_NAME_GAMEVARANDFEE, key);
+      return genericDB.getUintStorage(CONTRACT_NAME_GAMEVARANDFEE, HONEY_POT_DURATION);
   }
 
 }
