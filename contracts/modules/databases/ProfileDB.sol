390d58dddf91e3a8bc556ca4eab451f7792134e9
Mustafa Morca <psychoplasma@gmail.com>
2019-04-30 02:11:22 +0900

    Change table name paramter from string to byte32 in functions of GenereicDB and add new linkedlist type with address type node to GenericDB

--- contracts/modules/databases/ProfileDB.sol
+++ contracts/modules/databases/ProfileDB.sol
@@ -12,7 +12,6 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 pragma solidity ^0.5.5;
 
-import "../../Proxy.sol";
 import "../proxy/Proxied.sol";
 import "./GenericDB.sol";
 import "../../libs/SafeMath.sol";
@@ -27,9 +26,15 @@ contract ProfileDB is Proxied {
 
   GenericDB public genericDB;
 
-  string internal constant TABLE_NAME = "ProfileTable";
+  bytes32 internal constant TABLE_KEY_PROFILE = keccak256(abi.encodePacked("ProfileTable"));
+  string internal constant TABLE_NAME_KITTIE = "KittieTable";
   string internal constant ERROR_ALREADY_EXIST = "Profile already exists";
   string internal constant ERROR_DOES_NOT_EXIST = "Profile not exists";
+
+  modifier onlyExistentProfile(address account) {
+    require(doesProfileExist(account), ERROR_DOES_NOT_EXIST);
+    _;
+  }
 
   constructor(GenericDB _genericDB) public {
     setGenericDB(_genericDB);
@@ -39,57 +44,63 @@ contract ProfileDB is Proxied {
     genericDB = _genericDB;
   }
 
-  function create(uint256 _id)
+  function create(address account)
     external onlyContract(CONTRACT_NAME_REGISTER)
   {
     // Creates a linked list with the given keys, if it does not exist
     // And push the new profile pointer to the list
-    require(genericDB.pushNodeToLinkedList(CONTRACT_NAME_PROFILE_DB, TABLE_NAME, _id), ERROR_ALREADY_EXIST);
-  }
-
-  function setAccountAttributes(
-    uint256 _id,
-    address owner,
-    bytes calldata genes,
-    bytes calldata description
-  )
-    external onlyContract(CONTRACT_NAME_REGISTER)
-  {
-    require(genericDB.doesNodeExist(CONTRACT_NAME_PROFILE_DB, TABLE_NAME, _id), ERROR_DOES_NOT_EXIST);
-    genericDB.setAddressStorage(CONTRACT_NAME_PROFILE_DB, keccak256(abi.encodePacked(_id, "ownerAddress")), owner);
-    genericDB.setBytesStorage(CONTRACT_NAME_PROFILE_DB, keccak256(abi.encodePacked(_id, "description")), description);
-    genericDB.setBytesStorage(CONTRACT_NAME_PROFILE_DB, keccak256(abi.encodePacked(_id, "genes")), genes);
-  }
-
-  function setLoginStatus(uint256 _id, bool isLoggedIn)
-    external onlyContract(CONTRACT_NAME_REGISTER)
-  {
-    require(genericDB.doesNodeExist(CONTRACT_NAME_PROFILE_DB, TABLE_NAME, _id), ERROR_DOES_NOT_EXIST);
-    genericDB.setBoolStorage(CONTRACT_NAME_PROFILE_DB, keccak256(abi.encodePacked(_id, "isLoggedIn")), isLoggedIn);
+    require(genericDB.pushNodeToLinkedListAddr(CONTRACT_NAME_PROFILE_DB, TABLE_KEY_PROFILE, account), ERROR_ALREADY_EXIST);
   }
 
   function setKittieAttributes(
-    uint256 _id,
+    address account,
     uint256 kittieId,
-    uint256 kittieHash,
     uint256 deadAt,
-    string calldata kittieReferalHash,
     string calldata kittieStatus
   )
-    external onlyContract(CONTRACT_NAME_REGISTER)
+    external
+    onlyContract(CONTRACT_NAME_REGISTER)
+    onlyExistentProfile(account)
   {
-    require(genericDB.doesNodeExist(CONTRACT_NAME_PROFILE_DB, TABLE_NAME, _id), ERROR_DOES_NOT_EXIST);
-    uint256 numberOfKitties = genericDB.getUintStorage(CONTRACT_NAME_PROFILE_DB, keccak256(abi.encodePacked(_id, "kittieLength")));
-    genericDB.setUintStorage(CONTRACT_NAME_PROFILE_DB, keccak256(abi.encodePacked(_id, "kittieLength")), numberOfKitties.add(1));
-    genericDB.setUintStorage(CONTRACT_NAME_PROFILE_DB, keccak256(abi.encodePacked(_id, "kittieId")), kittieId);
-    genericDB.setUintStorage(CONTRACT_NAME_PROFILE_DB, keccak256(abi.encodePacked(_id, "kittieHash")), kittieHash);
-    genericDB.setUintStorage(CONTRACT_NAME_PROFILE_DB, keccak256(abi.encodePacked(_id, "deadAt")), deadAt);
-    genericDB.setStringStorage(CONTRACT_NAME_PROFILE_DB, keccak256(abi.encodePacked(_id, "kittieReferalHash")), kittieReferalHash);
-    genericDB.setStringStorage(CONTRACT_NAME_PROFILE_DB, keccak256(abi.encodePacked(_id, "kittieStatus")), kittieStatus);
+    // Check if kittie exists under this account
+    require(genericDB.doesNodeExist(CONTRACT_NAME_PROFILE_DB, keccak256(abi.encodePacked(account, TABLE_NAME_KITTIE)), kittieId));
+    genericDB.setUintStorage(CONTRACT_NAME_PROFILE_DB, keccak256(abi.encodePacked(account, kittieId, "deadAt")), deadAt);
+    genericDB.setStringStorage(CONTRACT_NAME_PROFILE_DB, keccak256(abi.encodePacked(account, kittieId, "kittieStatus")), kittieStatus);
+  }
+
+  function addKittie(
+    address account,
+    uint256 kittieId,
+    uint256 deadAt,
+    string calldata kittieStatus
+  )
+    external
+    onlyContract(CONTRACT_NAME_REGISTER)
+    onlyExistentProfile(account)
+  {
+    // Check if kittie exists or not under this account
+    // If it does not, add it to kittie table under this account
+    require(genericDB.pushNodeToLinkedList(CONTRACT_NAME_PROFILE_DB, keccak256(abi.encodePacked(account, TABLE_NAME_KITTIE)), kittieId));
+    // Set that kittie's attributes
+    genericDB.setUintStorage(CONTRACT_NAME_PROFILE_DB, keccak256(abi.encodePacked(account, kittieId, "deadAt")), deadAt);
+    genericDB.setStringStorage(CONTRACT_NAME_PROFILE_DB, keccak256(abi.encodePacked(account, kittieId, "kittieStatus")), kittieStatus);
+  }
+
+  function removeKittie(
+    address account,
+    uint256 kittieId
+  )
+    external
+    onlyContract(CONTRACT_NAME_REGISTER)
+    onlyExistentProfile(account)
+  {
+    // Check if kittie exists under this account
+    // If it exists, remove the kittie from kittie table under this account
+    require(genericDB.removeNodeFromLinkedList(CONTRACT_NAME_PROFILE_DB, keccak256(abi.encodePacked(account, TABLE_NAME_KITTIE)), kittieId));
   }
 
   function setGamingAttributes(
-    uint256 _id,
+    address account,
     uint256 totalWins,
     uint256 totalLosses,
     uint256 tokensWon,
@@ -97,60 +108,99 @@ contract ProfileDB is Proxied {
     uint256 feeHistory,
     bool isFreeToPlay
   )
-    external onlyContract(CONTRACT_NAME_REGISTER)
+    external
+    onlyContract(CONTRACT_NAME_REGISTER)
+    onlyExistentProfile(account)
   {
-    require(genericDB.doesNodeExist(CONTRACT_NAME_PROFILE_DB, TABLE_NAME, _id), ERROR_DOES_NOT_EXIST);
-    genericDB.setUintStorage(CONTRACT_NAME_PROFILE_DB, keccak256(abi.encodePacked(_id, "totalWins")), totalWins);
-    genericDB.setUintStorage(CONTRACT_NAME_PROFILE_DB, keccak256(abi.encodePacked(_id, "totalLosses")), totalLosses);
-    genericDB.setUintStorage(CONTRACT_NAME_PROFILE_DB, keccak256(abi.encodePacked(_id, "tokensWon")), tokensWon);
-    genericDB.setUintStorage(CONTRACT_NAME_PROFILE_DB, keccak256(abi.encodePacked(_id, "lastFeeDate")), lastFeeDate);
-    genericDB.setUintStorage(CONTRACT_NAME_PROFILE_DB, keccak256(abi.encodePacked(_id, "feeHistory")), feeHistory);
-    genericDB.setBoolStorage(CONTRACT_NAME_PROFILE_DB, keccak256(abi.encodePacked(_id, "isFreeToPlay")), isFreeToPlay);
+    genericDB.setUintStorage(CONTRACT_NAME_PROFILE_DB, keccak256(abi.encodePacked(account, "totalWins")), totalWins);
+    genericDB.setUintStorage(CONTRACT_NAME_PROFILE_DB, keccak256(abi.encodePacked(account, "totalLosses")), totalLosses);
+    genericDB.setUintStorage(CONTRACT_NAME_PROFILE_DB, keccak256(abi.encodePacked(account, "tokensWon")), tokensWon);
+    genericDB.setUintStorage(CONTRACT_NAME_PROFILE_DB, keccak256(abi.encodePacked(account, "lastFeeDate")), lastFeeDate);
+    genericDB.setUintStorage(CONTRACT_NAME_PROFILE_DB, keccak256(abi.encodePacked(account, "feeHistory")), feeHistory);
+    genericDB.setBoolStorage(CONTRACT_NAME_PROFILE_DB, keccak256(abi.encodePacked(account, "isFreeToPlay")), isFreeToPlay);
   }
 
   function setFightingAttributes(
-    uint256 _id,
+    address account,
     uint256 totalFights,
     uint256 nextFight,
     uint256 listingStart,
     uint256 listingEnd
   )
-    external onlyContract(CONTRACT_NAME_REGISTER)
+    external
+    onlyContract(CONTRACT_NAME_REGISTER)
+    onlyExistentProfile(account)
   {
-    require(genericDB.doesNodeExist(CONTRACT_NAME_PROFILE_DB, TABLE_NAME, _id), ERROR_DOES_NOT_EXIST);
-    genericDB.setUintStorage(CONTRACT_NAME_PROFILE_DB, keccak256(abi.encodePacked(_id, "totalFights")), totalFights);
-    genericDB.setUintStorage(CONTRACT_NAME_PROFILE_DB, keccak256(abi.encodePacked(_id, "nextFight")), nextFight);
-    genericDB.setUintStorage(CONTRACT_NAME_PROFILE_DB, keccak256(abi.encodePacked(_id, "listingStart")), listingStart);
-    genericDB.setUintStorage(CONTRACT_NAME_PROFILE_DB, keccak256(abi.encodePacked(_id, "listingEnd")), listingEnd);
+    genericDB.setUintStorage(CONTRACT_NAME_PROFILE_DB, keccak256(abi.encodePacked(account, "totalFights")), totalFights);
+    genericDB.setUintStorage(CONTRACT_NAME_PROFILE_DB, keccak256(abi.encodePacked(account, "nextFight")), nextFight);
+    genericDB.setUintStorage(CONTRACT_NAME_PROFILE_DB, keccak256(abi.encodePacked(account, "listingStart")), listingStart);
+    genericDB.setUintStorage(CONTRACT_NAME_PROFILE_DB, keccak256(abi.encodePacked(account, "listingEnd")), listingEnd);
   }
 
   function setFeeAttributes(
-    uint256 _id,
+    address account,
     uint256 feeType,
     uint256 paidDate,
     uint256 expirationDate,
     bool isPaid
   )
-    external onlyContract(CONTRACT_NAME_REGISTER)
+    external
+    onlyContract(CONTRACT_NAME_REGISTER)
+    onlyExistentProfile(account)
   {
-    require(genericDB.doesNodeExist(CONTRACT_NAME_PROFILE_DB, TABLE_NAME, _id), ERROR_DOES_NOT_EXIST);
-    genericDB.setUintStorage(CONTRACT_NAME_PROFILE_DB, keccak256(abi.encodePacked(_id, "feeType")), feeType);
-    genericDB.setUintStorage(CONTRACT_NAME_PROFILE_DB, keccak256(abi.encodePacked(_id, "paidDate")), paidDate);
-    genericDB.setUintStorage(CONTRACT_NAME_PROFILE_DB, keccak256(abi.encodePacked(_id, "expirationDate")), expirationDate);
-    genericDB.setBoolStorage(CONTRACT_NAME_PROFILE_DB, keccak256(abi.encodePacked(_id, "isPaid")), isPaid);
+    genericDB.setUintStorage(CONTRACT_NAME_PROFILE_DB, keccak256(abi.encodePacked(account, "feeType")), feeType);
+    genericDB.setUintStorage(CONTRACT_NAME_PROFILE_DB, keccak256(abi.encodePacked(account, "paidDate")), paidDate);
+    genericDB.setUintStorage(CONTRACT_NAME_PROFILE_DB, keccak256(abi.encodePacked(account, "expirationDate")), expirationDate);
+    genericDB.setBoolStorage(CONTRACT_NAME_PROFILE_DB, keccak256(abi.encodePacked(account, "isPaid")), isPaid);
   }
 
-  function setTokenEconomyAttributes(
-    uint256 _id,
-    uint256 kittieFightTokens,
+  function setSuperDAOTokens(
+    address account,
     uint256 superDAOTokens,
     bool isStakingSuperDAO
   )
-    external onlyContract(CONTRACT_NAME_REGISTER)
+    external
+    onlyContract(CONTRACT_NAME_REGISTER)
+    onlyExistentProfile(account)
   {
-    require(genericDB.doesNodeExist(CONTRACT_NAME_PROFILE_DB, TABLE_NAME, _id), ERROR_DOES_NOT_EXIST);
-    genericDB.setUintStorage(CONTRACT_NAME_PROFILE_DB, keccak256(abi.encodePacked(_id, "kittieFightTokens")), kittieFightTokens);
-    genericDB.setUintStorage(CONTRACT_NAME_PROFILE_DB, keccak256(abi.encodePacked(_id, "superDAOTokens")), superDAOTokens);
-    genericDB.setBoolStorage(CONTRACT_NAME_PROFILE_DB, keccak256(abi.encodePacked(_id, "isStakingSuperDAO")), isStakingSuperDAO);
+    genericDB.setUintStorage(CONTRACT_NAME_PROFILE_DB, keccak256(abi.encodePacked(account, "superDAOTokens")), superDAOTokens);
+    genericDB.setBoolStorage(CONTRACT_NAME_PROFILE_DB, keccak256(abi.encodePacked(account, "isStakingSuperDAO")), isStakingSuperDAO);
+  }
+
+  function setKittieFightTokens(
+    address account,
+    uint256 kittieFightTokens
+  )
+    external
+    onlyContract(CONTRACT_NAME_REGISTER)
+    onlyExistentProfile(account)
+  {
+    genericDB.setUintStorage(CONTRACT_NAME_PROFILE_DB, keccak256(abi.encodePacked(account, "kittieFightTokens")), kittieFightTokens);
+  }
+
+  function getKittieFightTokens(address account)
+    public
+    onlyExistentProfile(account)
+    view returns (uint256 amount)
+  {
+    amount = genericDB.getUintStorage(CONTRACT_NAME_PROFILE_DB, keccak256(abi.encodePacked(account, "kittieFightTokens")));
+  }
+
+  function doesProfileExist(address account) public view returns (bool) {
+    return genericDB.doesNodeAddrExist(CONTRACT_NAME_PROFILE_DB, TABLE_KEY_PROFILE, account);
+  }
+
+  function getKitties(address account) public view returns (uint256[] memory) {
+    return genericDB.getAll(
+      CONTRACT_NAME_PROFILE_DB,
+      keccak256(abi.encodePacked(account, TABLE_NAME_KITTIE))
+    );
+  }
+
+  function getKittieCount(address account) public view returns (uint256) {
+    return genericDB.getLinkedListSize(
+      CONTRACT_NAME_PROFILE_DB,
+      keccak256(abi.encodePacked(account, TABLE_NAME_KITTIE))
+    );
   }
 }
--- test/GenericDB.test.js
+++ test/GenericDB.test.js
@@ -1,6 +1,7 @@
 const GenericDB = artifacts.require('GenericDB');
 const Proxy = artifacts.require('Proxy');
 const BigNumber = require('bignumber.js');
+const { ZERO_ADDRESS } = require('./utils/constants');
 const CONTRACT_NAME = 'ProfileDB';
 const DB_TABLE_NAME = 'TestTable';
 
@@ -13,6 +14,7 @@ require('chai')
 
 contract('GenericDB', ([creator, unauthorizedAddr, randomAddr]) => {
   let nodeId = 12324353;
+  let addrNodeId = '0x0000000000000000000000000000000000000001';
 
   beforeEach(async () => {
     this.genericDB = await GenericDB.new();
@@ -36,17 +38,23 @@ contract('GenericDB', ([creator, unauthorizedAddr, randomAddr]) => {
 
     it('does not allow unauthorized address to access attribute setter functions', async () => {
       let key = web3.utils.soliditySha3('data_key');
+      let tableKey = web3.utils.soliditySha3(DB_TABLE_NAME);
       await this.genericDB.setIntStorage(CONTRACT_NAME, key, -123, {from: unauthorizedAddr}).should.be.rejected;
       await this.genericDB.setUintStorage(CONTRACT_NAME, key, 123, {from: unauthorizedAddr}).should.be.rejected;
       await this.genericDB.setAddressStorage(CONTRACT_NAME, key, randomAddr, {from: unauthorizedAddr}).should.be.rejected;
       await this.genericDB.setBoolStorage(CONTRACT_NAME, key, true, {from: unauthorizedAddr}).should.be.rejected;
       await this.genericDB.setBytesStorage(CONTRACT_NAME, key, web3.utils.toHex(123), {from: unauthorizedAddr}).should.be.rejected;
       await this.genericDB.setStringStorage(CONTRACT_NAME, key, 'asdad', {from: unauthorizedAddr}).should.be.rejected;
-      await this.genericDB.pushNodeToLinkedList(CONTRACT_NAME, DB_TABLE_NAME, nodeId, {from: unauthorizedAddr}).should.be.rejected;
+      await this.genericDB.pushNodeToLinkedList(CONTRACT_NAME, tableKey, nodeId, {from: unauthorizedAddr}).should.be.rejected;
+      await this.genericDB.pushNodeToLinkedListAddr(CONTRACT_NAME, tableKey, addrNodeId, {from: unauthorizedAddr}).should.be.rejected;
       
       // Create a linked list and an item in the list with authorized address to test the remaining functions
-      await this.genericDB.pushNodeToLinkedList(CONTRACT_NAME, DB_TABLE_NAME, nodeId).should.be.fulfilled;
-      await this.genericDB.removeNodeFromLinkedList(CONTRACT_NAME, DB_TABLE_NAME, nodeId, {from: unauthorizedAddr}).should.be.rejected;
+      await this.genericDB.pushNodeToLinkedList(CONTRACT_NAME, tableKey, nodeId).should.be.fulfilled;
+      await this.genericDB.removeNodeFromLinkedList(CONTRACT_NAME, tableKey, nodeId, {from: unauthorizedAddr}).should.be.rejected;
+
+      // Create a linked list(address) and an item in the list with authorized address to test the remaining functions
+      await this.genericDB.pushNodeToLinkedListAddr(CONTRACT_NAME, tableKey, addrNodeId).should.be.fulfilled;
+      await this.genericDB.removeNodeFromLinkedListAddr(CONTRACT_NAME, tableKey, addrNodeId, {from: unauthorizedAddr}).should.be.rejected;
     });
   });
 
@@ -83,36 +91,53 @@ contract('GenericDB', ([creator, unauthorizedAddr, randomAddr]) => {
     });
 
     it('creates and modifies linklist', async () => {
-      await this.genericDB.pushNodeToLinkedList(CONTRACT_NAME, DB_TABLE_NAME, nodeId).should.be.fulfilled;
-      let doesExist = await this.genericDB.doesNodeExist(CONTRACT_NAME, DB_TABLE_NAME, nodeId);
+      let tableKey = web3.utils.soliditySha3(DB_TABLE_NAME);
+
+      await this.genericDB.pushNodeToLinkedList(CONTRACT_NAME, tableKey, nodeId).should.be.fulfilled;
+      let doesExist = await this.genericDB.doesNodeExist(CONTRACT_NAME, tableKey, nodeId);
       doesExist.should.be.true;
 
-      doesExist = await this.genericDB.doesListExist(CONTRACT_NAME, DB_TABLE_NAME);
+      doesExist = await this.genericDB.doesListExist(CONTRACT_NAME, tableKey);
       doesExist.should.be.true;
 
-      await this.genericDB.removeNodeFromLinkedList(CONTRACT_NAME, DB_TABLE_NAME, nodeId).should.be.fulfilled;
-      doesExist = await this.genericDB.doesNodeExist(CONTRACT_NAME, DB_TABLE_NAME, nodeId);
+      await this.genericDB.removeNodeFromLinkedList(CONTRACT_NAME, tableKey, nodeId).should.be.fulfilled;
+      doesExist = await this.genericDB.doesNodeExist(CONTRACT_NAME, tableKey, nodeId);
       doesExist.should.be.false;
 
-      doesExist = await this.genericDB.doesListExist(CONTRACT_NAME, DB_TABLE_NAME);
+      doesExist = await this.genericDB.doesListExist(CONTRACT_NAME, tableKey);
+      doesExist.should.be.false;
+
+      await this.genericDB.pushNodeToLinkedListAddr(CONTRACT_NAME, tableKey, addrNodeId).should.be.fulfilled;
+      doesExist = await this.genericDB.doesNodeAddrExist(CONTRACT_NAME, tableKey, addrNodeId);
+      doesExist.should.be.true;
+
+      doesExist = await this.genericDB.doesListAddrExist(CONTRACT_NAME, tableKey);
+      doesExist.should.be.true;
+
+      await this.genericDB.removeNodeFromLinkedListAddr(CONTRACT_NAME, tableKey, addrNodeId).should.be.fulfilled;
+      doesExist = await this.genericDB.doesNodeAddrExist(CONTRACT_NAME, tableKey, addrNodeId);
+      doesExist.should.be.false;
+
+      doesExist = await this.genericDB.doesListAddrExist(CONTRACT_NAME, tableKey);
       doesExist.should.be.false;
     });
 
     it('iterates all items in linklist', async () => {
+      let tableKey = web3.utils.soliditySha3(DB_TABLE_NAME);
       let nodeIds = [1234, 45667, 34456, 342452, 123178];
 
       // First create some nodes in DB
       for (let i = 0; i < nodeIds.length; i++) {
-        await this.genericDB.pushNodeToLinkedList(CONTRACT_NAME, DB_TABLE_NAME, nodeIds[i]).should.be.fulfilled;
+        await this.genericDB.pushNodeToLinkedList(CONTRACT_NAME, tableKey, nodeIds[i]).should.be.fulfilled;
       }
 
-      let totalUsers = (await this.genericDB.getLinkedListSize(CONTRACT_NAME, DB_TABLE_NAME)).toNumber();
+      let totalUsers = (await this.genericDB.getLinkedListSize(CONTRACT_NAME, tableKey)).toNumber();
       totalUsers.should.be.equal(nodeIds.length);
 
       let node = 0; // Start from the HEAD. HEAD is always 0.
       let index = totalUsers - 1;
       do {
-        let ret = await this.genericDB.getAdjacent(CONTRACT_NAME, DB_TABLE_NAME, node, true);
+        let ret = await this.genericDB.getAdjacent(CONTRACT_NAME, tableKey, node, true);
         // ret value includes direction and node id. Ex => {'0': true, '1': 1234}
         node = ret['1'].toNumber();
 
@@ -122,6 +147,85 @@ contract('GenericDB', ([creator, unauthorizedAddr, randomAddr]) => {
         node.should.be.equal(nodeIds[index]);
         index--;
       } while (node)
+    });
+
+    it('gets all items in linklist', async () => {
+      let tableKey = web3.utils.soliditySha3(DB_TABLE_NAME);
+      let nodeIds = [1234, 45667, 34456, 342452, 123178];
+
+      // First create some nodes in DB
+      for (let i = 0; i < nodeIds.length; i++) {
+        await this.genericDB.pushNodeToLinkedList(CONTRACT_NAME, tableKey, nodeIds[i]).should.be.fulfilled;
+      }
+
+      let totalUsers = (await this.genericDB.getLinkedListSize(CONTRACT_NAME, tableKey)).toNumber();
+      totalUsers.should.be.equal(nodeIds.length);
+
+      let allNodes = await this.genericDB.getAll(CONTRACT_NAME, tableKey);
+
+      allNodes.map((node, index) => {
+        // The nodes are aquired in first in last out order
+        node.toNumber().should.be.equal(nodeIds[nodeIds.length - 1 - index]);
+      });
+    });
+
+    it('iterates all items in linklistAddr', async () => {
+      let tableKey = web3.utils.soliditySha3(DB_TABLE_NAME);
+      let addrNodeIds = [
+        '0x0000000000000000000000000000000000000001',
+        '0x0000000000000000000000000000000000000002',
+        '0x0000000000000000000000000000000000000003',
+        '0x0000000000000000000000000000000000000004',
+        '0x0000000000000000000000000000000000000005'
+      ];
+
+      // First create some nodes in DB
+      for (let i = 0; i < addrNodeIds.length; i++) {
+        await this.genericDB.pushNodeToLinkedListAddr(CONTRACT_NAME, tableKey, addrNodeIds[i]).should.be.fulfilled;
+      }
+
+      let totalUsers = (await this.genericDB.getLinkedListAddrSize(CONTRACT_NAME, tableKey)).toNumber();
+      totalUsers.should.be.equal(addrNodeIds.length);
+
+      let node = ZERO_ADDRESS; // Start from the HEAD. HEAD is always 0x0.
+      let index = totalUsers - 1;
+      do {
+        let ret = await this.genericDB.getAdjacentAddr(CONTRACT_NAME, tableKey, node, true);
+        // ret value includes direction and node id. Ex => {'0': true, '1': 0xa0a214...}
+        node = ret['1'];
+
+        // it means that we reach the end of the list
+        if (node === ZERO_ADDRESS) break;
+        // The first item in TRUE direction is the last item pushed => LIFO (stack)
+        node.should.be.equal(addrNodeIds[index]);
+        index--;
+      } while (node)
+    });
+
+    it('gets all items in linklistAddr', async () => {
+      let tableKey = web3.utils.soliditySha3(DB_TABLE_NAME);
+      let addrNodeIds = [
+        '0x0000000000000000000000000000000000000001',
+        '0x0000000000000000000000000000000000000002',
+        '0x0000000000000000000000000000000000000003',
+        '0x0000000000000000000000000000000000000004',
+        '0x0000000000000000000000000000000000000005'
+      ];
+
+      // First create some nodes in DB
+      for (let i = 0; i < addrNodeIds.length; i++) {
+        await this.genericDB.pushNodeToLinkedListAddr(CONTRACT_NAME, tableKey, addrNodeIds[i]).should.be.fulfilled;
+      }
+
+      let totalUsers = (await this.genericDB.getLinkedListAddrSize(CONTRACT_NAME, tableKey)).toNumber();
+      totalUsers.should.be.equal(addrNodeIds.length);
+
+      let allAddrNodes = await this.genericDB.getAllAddr(CONTRACT_NAME, tableKey);
+
+      allAddrNodes.map((node, index) => {
+        // The nodes are aquired in first in last out order
+        node.should.be.equal(addrNodeIds[addrNodeIds.length - 1 - index]);
+      });
     });
   });
 });
