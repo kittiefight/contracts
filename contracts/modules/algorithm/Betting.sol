66803704f8e2ea474ed8aeebbdecb042900bed64
“wafflemakr” <wafflemakr@gmail.com>
2019-06-27 14:14:51 -0500

    add betting contract structure

--- contracts/modules/algorithm/Betting.sol
+++ contracts/modules/algorithm/Betting.sol
@@ -1,7 +1,7 @@
 /**
  * @title Betting
  *
- * @author @kittieFIGHT @ola
+ * @author @wafflemakr
  *
  */
 //modifier class (DSAuth )
@@ -21,5 +21,53 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 pragma solidity ^0.5.5;
 
-contract Betting {
+import '../proxy/Proxied.sol';
+import './HitsResolveAlgo.sol';
+import '../databases/GameManagerDB.sol';
+
+contract Betting is Proxied {
+
+    //Contract Variables
+    GameManagerDB public gameManagerDB;
+    HitsResolve public hitsResolve;
+
+
+    /**
+    * @dev Sets related contracts
+    * @dev Can be called only by the owner of this contract
+    */
+    function initialize() external onlyOwner {
+
+        //TODO: Check what other contracts do we need
+        gameManagerDB = GameManagerDB(proxy.getContract(CONTRACT_NAME_GAMEMANAGER_DB));
+        hitsResolve = HitsResolve(proxy.getContract(CONTRACT_NAME_HITSRESOLVE));
+    }
+
+    function startGame(uint randomNum)
+        external
+        onlyContract(CONTRACT_NAME_GAMEMANAGER)
+        returns(uint[] memory)
+    {
+        //return FightMap
+    }
+
+    function bet(uint gameId, uint randomNum, uint betAmount)
+        external
+        onlyContract(CONTRACT_NAME_GAMEMANAGER)
+    {
+        //get last five bets from GameManagerDB
+        //check if bet is greater than previous 5 bets
+        
+        storeRandomSeed(randomNum);
+
+        // prevent supporting address from betting on other side already, done in GameManagerDB
+        // calls endowment api : send KTY token fee to endowment fund, done in GameManager
+
+        //return (attackHash, attackType);
+    }
+
+    function storeRandomSeed(uint randomNum) internal{
+        //uint currentRandom = hitsResolve.calculateCurrentRandom(randomNum);
+    }
+
 }
