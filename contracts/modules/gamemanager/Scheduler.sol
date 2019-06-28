6ceaedeced5010441f2ef1b672173182c6e7302d
vmandal <vikram@t3in.com>
2019-06-28 08:39:35 +0530

    code improvement and getters

--- contracts/modules/gamemanager/Scheduler.sol
+++ contracts/modules/gamemanager/Scheduler.sol
@@ -40,18 +40,15 @@ contract Scheduler is Proxied {
     //Contract Variables
     GameManager public gameManager;
     GameVarAndFee public gameVarAndFee;
-    //Register public register;
 
-    uint256 randomNonce;
-
-    uint256 kittyCount;
     struct Kitty {
         uint256 kittyId;
         address player;
     }
-    Kitty[] KittyList;
-    Kitty[] KittyListSuffled;
 
+    uint256 randomNonce;
+    Kitty[] kittyList;
+    Kitty[] kittyListSuffled;
 
     /**
      * @dev Can be called only by the owner of this contract
@@ -64,77 +61,125 @@ contract Scheduler is Proxied {
     /**
     * @dev what modifier to add? onlyValidPlayer
     * @param _kittyId kitty id
-    * @param _player is the address of the palyer
+    * @param _player is the address of the player
     */
     function addKittyToList(uint256 _kittyId, address _player) external onlyContract(CONTRACT_NAME_GAMEMANAGER){
+        require(!isKittyListedForMatching(_kittyId), "Kitty is already listed for upcomming games");
+        kittyList[kittyList.length] = Kitty({ kittyId: _kittyId, player: _player });
 
-        KittyList[kittyCount] = Kitty({ kittyId: _kittyId, player: _player });
-        kittyCount++;
-
-        // call matchKitties - check with GameVarFee getRequiredNumberMatches()
-        if (gameVarAndFee.getRequiredNumberMatches() == kittyCount) {
+        if (gameVarAndFee.getRequiredNumberMatches() == (kittyList.length * 2) ) {
             matchKitties();
         }
+    }
 
+    /**
+    * @dev Checkes if kitty is listed for matching in future games
+    */
+    function isKittyListedForMatching(uint256 _kittyId) public view returns (bool) {
+        uint256[] memory unListed = getUnMatchedKitties();
+        for(uint256 i = 0; i < unListed.length ; i++){
+            if (_kittyId == unListed[i]){
+                return true;
+            }
+        }
+        return false;
     }
 
 
     /**
-     * @dev this will be called by GameManager::matchKitties()
+     * @dev Create red and black corner players
      */
     function matchKitties() private {
-        require((kittyCount % 2) != 0, "Kitty count should be even number");
+        require((kittyList.length % 2) != 0, "Number of Kitties should be even number");
 
         Kitty[] memory playerRed;
         Kitty[] memory playerBlack;
-        uint256 gameCount = kittyCount / 2;
 
         suffleKittyList();
+        uint256 gameCount = kittyListSuffled.length / 2;
 
         for(uint256 i = 0; i < gameCount; i++){
-            playerRed[i] = KittyListSuffled[i];
+            playerRed[i] = kittyListSuffled[i];
         }
-        for(uint256 i = gameCount - 1; i < kittyCount; i++){
-            playerBlack[i] = KittyListSuffled[i];
+        for(uint256 i = gameCount - 1; i < kittyListSuffled.length; i++){
+            playerBlack[i] = kittyListSuffled[i];
         }
 
-        uint gameCreationTime;
-        uint gameTimeSeperation = gameVarAndFee.getGameTimes(); //OR getRequiredTimeDistance() ?
+        uint256 gameCreationTime;
+        uint256 gameTimeSeperation = gameVarAndFee.getGameTimes(); //OR getRequiredTimeDistance() ?
 
         //generate match pairs
-        for(uint i = 0; i < gameCount; i++){
+        for(uint256 i = 0; i < gameCount; i++){
             gameCreationTime = block.timestamp + (gameTimeSeperation * 1 seconds);
-
-            // createFight not public
-            //gameManager.createFight(playerRed[i].owner, playerBlack[i].owner,  playerRed[i].kittyId, playerBlack[i].kittyId, gameCreationTime);
-
+            gameManager.createFight(playerRed[i].player, playerBlack[i].player,  playerRed[i].kittyId, playerBlack[i].kittyId, gameCreationTime);
         }
 
-        // reset KittyList, KittyListSuffled, kittyCount
-        kittyCount = 0;
-        delete KittyList;
-        delete KittyListSuffled;
+        delete kittyListSuffled; // reset
     }
-
 
     /**
      * needs to be tested
      */
     function suffleKittyList() internal {
+
+        Kitty[] memory kittyListCopy;
+        kittyListCopy = kittyList;
+        uint256 suffleKittyListCount = kittyList.length;
+        delete kittyList;   // reset
+
         uint256 pos;
-        for(uint256 i = 0; i < kittyCount; i++){
-            pos = randomNumber(kittyCount);
-            KittyListSuffled[pos] = KittyList[i];
+        for(uint256 i = 0; i < suffleKittyListCount; i++){
+            pos = randomNumber(suffleKittyListCount);
+            kittyListSuffled[pos] = kittyListCopy[i];
         }
     }
 
     /**
-     * needs to be tested
+     * @dev Random number - very basic implementation
      */
     function randomNumber(uint256 max) internal returns (uint256){
         uint256 random = uint256(keccak256(abi.encodePacked(block.timestamp, msg.sender, randomNonce))) % max;
         randomNonce++;
         return random;
     }
+
+    /**
+     * @return uint256[] Returns only ids of currently un mathced kitties
+     */
+    function getUnMatchedKitties() public view returns (uint256[] memory){
+        uint256[] memory unMatchedKitties;
+        for (uint256 i = 0; i < kittyList.length; i++){
+            unMatchedKitties[i] = kittyList[i].kittyId;
+        }
+        return unMatchedKitties;
+    }
+
+    /**
+     * @return address[] Returns addresses  of currently un mathced players
+     */
+    function getUnMatchedPlayers() public view returns (address[] memory){
+        address[] memory unMatchedPlayers;
+        for (uint256 i = 0; i < kittyList.length; i++){
+            unMatchedPlayers[i] = kittyList[i].player;
+        }
+        return unMatchedPlayers;
+    }
+
+    /**
+     * return requiredNumber of listed kitties required before the next nbatches of fights is setup
+     */
+    function getRequiredMatchingnumber(uint256 nbatch) external view returns(uint256){
+        uint256[] memory currentUnMatchedKitties = getUnMatchedKitties();
+        return  (nbatch * 2) - currentUnMatchedKitties.length;
+    }
+
+    /**
+     * @dev internal getter to return requiredtime distance between all fights
+     * Is it required?
+     * /
+    function getRequiredTimeDistance() external view returns(uint256){
+        return gameVarAndFee.getGameTimes();
+    }*/
+
 
 }
