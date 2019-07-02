c66326da03704941b7f952705c5b5165a3505feb
“wafflemakr” <wafflemakr@gmail.com>
2019-07-02 18:48:17 -0500

    change time library to BokkyPooBahs

--- contracts/modules/datetime/DateTime.sol
+++ contracts/modules/datetime/DateTime.sol
@@ -5,19 +5,35 @@
 *
 */
 
+// Conventions:
+// Unit      | Range         | Notes
+// :-------- |:-------------:|:-----
+// timestamp | >= 0          | Unix timestamp, number of seconds since 1970/01/01 00:00:00 UTC
+// year      | 1970 ... 2345 |
+// month     | 1 ... 12      |
+// day       | 1 ... 31      |
+// hour      | 0 ... 23      |
+// minute    | 0 ... 59      |
+// second    | 0 ... 59      |
+// dayOfWeek | 1 ... 7       | 1 = Monday, ..., 7 = Sunday
+
 pragma solidity ^0.5.5;
 
-import "./DateTimeAPI.sol";
+
 import "../../GameVarAndFee.sol";
 import "../proxy/Proxied.sol";
+import "./BokkyPooBahsDateTimeLibrary.sol";
+
 
 /**
- * @title Contract to track minutes, hour, daily, the weekly and monthly schedule 
+ * @title Contract to track minutes, hour, daily, the weekly and monthly schedule
  * for scheduling activities within the platform. Used by contract to for time tracking,
  * accurately scheduling game events within the kittiefight sytem .
  * @dev check return variables, depending of what other contracts need.
  */
-contract DateTime is Proxied, DateTimeAPI {
+contract DateTime is Proxied {
+
+    using BokkyPooBahsDateTimeLibrary for uint;
 
     GameVarAndFee public gameVarAndFee;
 
@@ -34,82 +50,71 @@ contract DateTime is Proxied, DateTimeAPI {
     function convertTimeStamp(uint timeStamp)
         public pure
         returns(
-        uint16 _year, uint8 _month, uint8 _day, uint8 _hour,
-        uint8 _minute, uint8 second, uint8 weekday)
+        uint _year, uint _month, uint _day, uint _hour,
+        uint _minute, uint second)
     {
-        _DateTime memory dt = parseTimestamp(timeStamp);
-        return (dt.year, dt.month, dt.day, dt.hour, dt.minute, dt.second, dt.weekday);
+        (_year, _month, _day, _hour, _minute, second) = BokkyPooBahsDateTimeLibrary.timestampToDateTime(timeStamp);
     }
 
     
     /**
-    * @notice Uses the variable "gameDuration" to compare against current time and 
+    * @notice Uses the variable "gameDuration" to compare against current time and
     * determine the "Time" when game should end .
     * @return Time in hour, minutes and seconds
     */
-    function runGameDurationTime() 
-    public view 
-    returns(
-    uint8 _hour, uint8 _minute, uint8 second)
-    {        
+    function runGameDurationTime()
+        public view
+        returns(
+        uint _year, uint _month, uint _day, uint _hour,
+        uint _minute, uint second)
+    {
         uint _gameDuration = gameVarAndFee.getGameDuration();
-
-        _DateTime memory dt = parseTimestamp(now + _gameDuration);
-
-        return (dt.hour, dt.minute, dt.second);
+        (_year, _month, _day, _hour, _minute, second) = BokkyPooBahsDateTimeLibrary.timestampToDateTime(now + _gameDuration);
 
     }
 
     /**
-    * @notice Uses the variable "kittieHellExpiration" to compare against gameEndtime and 
-    * determine  the "Time" when a CAT in kittiehell is lost forever 
+    * @notice Uses the variable "kittieHellExpiration" to compare against gameEndtime and
+    * determine  the "Time" when a CAT in kittiehell is lost forever
     * @return Time in hour, minutes and seconds
     */
-    function runKittieHellExpirationTime(uint _gameEndTime) 
+    function runKittieHellExpirationTime(uint _gameEndTime)
     public view
-    returns(
-    uint8 _hour, uint8 _minute, uint8 second)
-    {        
+        returns(
+        uint _year, uint _month, uint _day, uint _hour,
+        uint _minute, uint second)
+    {
         uint _kittieHellExpiration = gameVarAndFee.getKittieHellExpiration();
-
-        _DateTime memory dt = parseTimestamp(_gameEndTime + _kittieHellExpiration);
-
-        return (dt.hour, dt.minute, dt.second);
-
+        (_year, _month, _day, _hour, _minute, second) = BokkyPooBahsDateTimeLibrary.timestampToDateTime(_gameEndTime + _kittieHellExpiration);
     }
 
     /**
-    * @notice Uses the variable "honeypotExpiration" to compare against gameEndtime and 
+    * @notice Uses the variable "honeypotExpiration" to compare against gameEndtime and
     * determine  the "Time" when honeypot should expire and be dissolved
     * @return Time in hour, minutes and seconds
     */
-    function runhoneypotExpirationTime(uint _gameEndTime) 
-    public view 
+    function runhoneypotExpirationTime(uint _gameEndTime)
+    public view
     returns(
-    uint8 _hour, uint8 _minute, uint8 second)
+        uint _year, uint _month, uint _day, uint _hour,
+        uint _minute, uint second)
     {
         uint _honeypotExpiration = gameVarAndFee.getHoneypotExpiration();
-
-        _DateTime memory dt = parseTimestamp(_gameEndTime + _honeypotExpiration);
-
-        return (dt.hour, dt.minute, dt.second);
-    } 
+        (_year, _month, _day, _hour, _minute, second) = BokkyPooBahsDateTimeLibrary.timestampToDateTime(_gameEndTime + _honeypotExpiration);
+    }
 
     /**
-    * @notice Uses the variable "futureGameTime" to compare against current time "now" 
+    * @notice Uses the variable "futureGameTime" to compare against current time "now"
     * and determine a future start date and or time for a game
     * @return Date and Time
     */
-    function runfutureGameTime() 
-    public view 
-    returns(
-    uint16 _year, uint8 _month, uint8 _day, uint8 _hour, 
-    uint8 _minute, uint8 second, uint8 weekday) 
+    function runfutureGameTime()
+        public view
+        returns(
+        uint _year, uint _month, uint _day, uint _hour,
+        uint _minute, uint second)
     {
         uint _futureGameTime = gameVarAndFee.getFutureGameTime();
-
-        _DateTime memory dt = parseTimestamp(now + _futureGameTime);
-
-        return (dt.year, dt.month, dt.day, dt.hour, dt.minute, dt.second, dt.weekday);
-    }  
+        (_year, _month, _day, _hour, _minute, second) = BokkyPooBahsDateTimeLibrary.timestampToDateTime(now + _futureGameTime);
+    }
 }
