1034f98271d3cd4bb3741c049d2d57ca23b519eb
vmandal <vikram@t3in.com>
2019-07-09 10:18:22 +0530

    copied lastest code from feature/proxy branch

--- contracts/authority/Guard.sol
+++ contracts/authority/Guard.sol
@@ -2,10 +2,11 @@ pragma solidity ^0.5.5;
 
 import "./SystemRoles.sol";
 import "../modules/databases/RoleDB.sol";
-import "../modules/proxy/ProxyBase.sol";
+import "../modules/proxy/Proxied.sol";
 
-contract Guard is ProxyBase, SystemRoles {
-  
+
+contract Guard is Proxied, SystemRoles {
+
   modifier onlySuperAdmin() {
     assert(msg.sender != address(0));
     require(checkRole(SUPER_ADMIN_ROLE), "Only super admin");
@@ -31,6 +32,21 @@ contract Guard is ProxyBase, SystemRoles {
   }
 
   function checkRole(string memory role) internal view returns (bool) {
-    return RoleDB(addressOfRoleDB()).hasRole(role, msg.sender);
+    return RoleDB(proxy.getContract(CONTRACT_NAME_ROLE_DB)).hasRole(role, getOriginalSender());
   }
+
+  function getOriginalSender() view internal returns(address){
+    if(msg.sender != address(proxy)) return msg.sender;
+    //Find out actual sender    
+    address sender;
+    assembly {
+        let ptr := sub(calldatasize, 20)  // Find out start position of the sender's address
+        mstore(0x20, 0)                   // Fill 32 bytes with 0
+        calldatacopy(0x2C, ptr, 20)       // Load 20 bytes of address to the end of cleared memory 
+        sender := mload(0x20)             // Store address to solidity variable
+    }
+    assert(sender != address(0));
+    return sender;
+  }
+
 }
