133b4d7f4d8d1d53c3b003e73fba9155574dfa5c
“wafflemakr” <wafflemakr@gmail.com>
2019-07-17 16:21:20 -0500

    remove library, update getters

--- contracts/modules/datetime/DateTime.sol
+++ contracts/modules/datetime/DateTime.sol
@@ -19,17 +19,13 @@
 
 pragma solidity ^0.5.5;
 
-
 import "../../GameVarAndFee.sol";
 import "../proxy/Proxied.sol";
-// import "./BokkyPooBahsDateTimeLibrary.sol";
-
 
 /**
  * @title Contract to track minutes, hour, daily, the weekly and monthly schedule
  * for scheduling activities within the platform. Used by contract to for time tracking,
  * accurately scheduling game events within the kittiefight sytem .
- * @dev check return variables, depending of what other contracts need.
  */
 contract DateTime is Proxied {
 
@@ -47,8 +43,13 @@ contract DateTime is Proxied {
         gameVarAndFee = GameVarAndFee(proxy.getContract(CONTRACT_NAME_GAMEVARANDFEE));
     }
 
-    function getBlockchainTime() public view returns(uint){
-        return now;
+    function getBlockchainTime()
+        public view
+        returns(
+        uint _year, uint _month, uint _day, uint _hour,
+        uint _minute, uint second)
+    {
+        (_year, _month, _day, _hour, _minute, second) = timestampToDateTime(now);
     }
 
     /**
@@ -63,68 +64,7 @@ contract DateTime is Proxied {
         (_year, _month, _day, _hour, _minute, second) = timestampToDateTime(timeStamp);
     }
 
-    
-    // /**
-    // * @notice Uses the variable "gameDuration" to compare against current time and
-    // * determine the "Time" when game should end .
-    // * @return Time in hour, minutes and seconds
-    // */
-    // function runGameDurationTime()
-    //     public view
-    //     returns(
-    //     uint _year, uint _month, uint _day, uint _hour,
-    //     uint _minute, uint second)
-    // {
-    //     uint _gameDuration = gameVarAndFee.getGameDuration();
-    //     (_year, _month, _day, _hour, _minute, second) = timestampToDateTime(now + _gameDuration);
-
-    // }
-
-    // /**
-    // * @notice Uses the variable "kittieHellExpiration" to compare against gameEndtime and
-    // * determine  the "Time" when a CAT in kittiehell is lost forever
-    // * @return Time in hour, minutes and seconds
-    // */
-    // function runKittieHellExpirationTime(uint _gameEndTime)
-    // public view
-    //     returns(
-    //     uint _year, uint _month, uint _day, uint _hour,
-    //     uint _minute, uint second)
-    // {
-    //     uint _kittieHellExpiration = gameVarAndFee.getKittieHellExpiration();
-    //     (_year, _month, _day, _hour, _minute, second) = timestampToDateTime(_gameEndTime + _kittieHellExpiration);
-    // }
-
-    // /**
-    // * @notice Uses the variable "honeypotExpiration" to compare against gameEndtime and
-    // * determine  the "Time" when honeypot should expire and be dissolved
-    // * @return Time in hour, minutes and seconds
-    // */
-    // function runhoneypotExpirationTime(uint _gameEndTime)
-    // public view
-    // returns(
-    //     uint _year, uint _month, uint _day, uint _hour,
-    //     uint _minute, uint second)
-    // {
-    //     uint _honeypotExpiration = gameVarAndFee.getHoneypotExpiration();
-    //     (_year, _month, _day, _hour, _minute, second) = timestampToDateTime(_gameEndTime + _honeypotExpiration);
-    // }
-
-    // /**
-    // * @notice Uses the variable "futureGameTime" to compare against current time "now"
-    // * and determine a future start date and or time for a game
-    // * @return Date and Time
-    // */
-    // function runfutureGameTime()
-    //     public view
-    //     returns(
-    //     uint _year, uint _month, uint _day, uint _hour,
-    //     uint _minute, uint second)
-    // {
-    //     uint _futureGameTime = gameVarAndFee.getFutureGameTime();
-    //     (_year, _month, _day, _hour, _minute, second) = timestampToDateTime(now + _futureGameTime);
-    // }
-
+    // from BokkyPooBahsDateTimeLibrary
     function timestampToDateTime(uint timestamp) internal pure returns (uint year, uint month, uint day, uint hour, uint minute, uint second) {
         (year, month, day) = _daysToDate(timestamp / SECONDS_PER_DAY);
         uint secs = timestamp % SECONDS_PER_DAY;
