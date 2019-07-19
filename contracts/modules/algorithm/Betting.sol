53be1353561372a889a0b96871b934ce547c1f1e
ziweidream <ziweidream@gmail.com>
2019-07-19 11:44:53 -0400

    add NatSpec comments

--- contracts/modules/algorithm/Betting.sol
+++ contracts/modules/algorithm/Betting.sol
@@ -25,28 +25,40 @@ import "../proxy/Proxied.sol";
 import "../../authority/Guard.sol";
 import "../../libs/SafeMath.sol";
 
+/**
+ * @title This contract is responsible to carry out the betting module in a game
+ * @author @ziweidream
+ */
+
 contract Betting is Proxied, Guard {
     using SafeMath for uint256;
 
-    //fight map for a game with a specific gameId
+    ///fight map for a game with a specific gameId
      struct HitType {
         bytes32 hash;
         string attack;
      }
     mapping(uint256 => HitType[]) public fightMap;
 
-    // total number of direct attacks of each hitType of the given corner in a game
+    /// total number of direct attacks of each hitType of the given corner in a game
      mapping(uint256 => mapping(address => uint256[7])) public directAttacksScored;
-     // total number of blocked attacks of each hitType of the given corner in a game
+    /// total number of blocked attacks of each hitType of the given corner in a game
     mapping(uint256 => mapping(address => uint256[7])) public blockedAttacksScored;
-    // individual bets placed by the given corner in a game
+    /// individual bets placed by the given corner in a game
     mapping(uint256 => mapping(address => uint256[])) public bets;
-   // timestamp of the last bet placed by the given corner in a game
+    /// timestamp of the last bet placed by the given corner in a game
     mapping(uint256 => mapping(address => uint256)) public lastBetTimestamp;
-    // current defense level of the given corner in a game
+    /// current defense level of the given corner in a game
     mapping(uint256 => mapping(address => uint256)) public defenseLevel;
 
     // setFightMap() is internal. Temporarily set as public for truffle test purpose.
+    /**
+     * @author @ziweidream
+     * @notice set fight map for a game with a specific gameId
+     * @param _gameId the gameID of the game for which the fight map is generated
+     * @param _randomRed random number generated when the red corner presses Button start
+     * @param _randomBlack black number generated when the black corner presses Button start
+     */
     function setFightMap(uint256 _gameId, uint256 _randomRed, uint256 _randomBlack) public {
         uint randomNum = _randomRed.add(_randomBlack);
         bytes32 hashLowPunch = keccak256(abi.encodePacked(randomNum, "lowPunch"));
@@ -66,42 +78,62 @@ contract Betting is Proxied, Guard {
     }
 
     // setDirectAttacksScored() is internal. Temporarily set as public for truffle test purpose.
-    // set the total number of direct attacks of each hitType of the given corner in a game
+    /**
+     * @author @ziweidream
+     * @notice record the total number of direct attacks of each hitType of the given corner in a game
+     * @param _gameId the gameID of the game
+     * @param _player the given corner in this game for whom the total number of the direct attacks are recorded
+     * @param index the index in the mapping directAttacksScored. The index is obtained in function getAttackType()
+     */
     function setDirectAttacksScored(
           uint256 _gameId,
-          address _supportedPlayer,
+          address _player,
           uint256 index
           )
           public {
-            directAttacksScored[_gameId][_supportedPlayer][index] = directAttacksScored[_gameId][_supportedPlayer][index].add(1);
+            directAttacksScored[_gameId][_player][index] = directAttacksScored[_gameId][_player][index].add(1);
           }
 
     // setBlockedAttacksScored() is internal. Temporarily set as public for truffle test purpose.
-    // set the total number of blocked attacks of each hitType of the given corner in a game
+    /**
+     * @author @ziweidream
+     * @notice record the total number of blocked attacks of each hitType of the given corner in a game
+     * @param _gameId the gameID of the game
+     * @param _player the given corner in this game for whom the total number of the blocked attacks are recorded
+     * @param index the index in the mapping blockedAttacksScored. The index is obtained in function getAttackType()
+     */
     function setBlockedAttacksScored(
           uint256 _gameId,
-          address _supportedPlayer,
+          address _player,
           uint256 index
           )
           public {
-            blockedAttacksScored[_gameId][_supportedPlayer][index] = blockedAttacksScored[_gameId][_supportedPlayer][index].add(1);
+            blockedAttacksScored[_gameId][_player][index] = blockedAttacksScored[_gameId][_player][index].add(1);
           }
-      
-      // get the total number of direct attacks of each hitType of the given corner in a game
-    function getDirectAttacksScored(uint256 _gameId, address _supportedPlayer)
+
+    // get the total number of direct attacks of each hitType of the given corner in a game
+    /**
+     * @author @ziweidream
+     * @notice get the total number of direct attacks of each hitType of the given corner in a game
+     * @param _gameId the gameID of the game
+     * @param _player the given corner in this game for whom the total number of the direct attacks are obtained
+     * @return an array of fixed size 7 directAttacks, which contains the total number of direct attacks of each hitType
+     * of the given corner in this game
+     */
+    function getDirectAttacksScored(uint256 _gameId, address _player)
         public
         view
         returns(
             uint256[7] memory directAttacks
             )
         {
-            uint256 totalLowPunch = directAttacksScored[_gameId][_supportedPlayer][0];
-            uint256 totalLowKick = directAttacksScored[_gameId][_supportedPlayer][1];
-            uint256 totalLowThunder = directAttacksScored[_gameId][_supportedPlayer][2];
-            uint256 totalHardPunch = directAttacksScored[_gameId][_supportedPlayer][3];
-            uint256 totalHardKick = directAttacksScored[_gameId][_supportedPlayer][4];
-            uint256 totalHardThunder = directAttacksScored[_gameId][_supportedPlayer][5];
-            uint256 totalSlash = directAttacksScored[_gameId][_supportedPlayer][6];
+            uint256 totalLowPunch = directAttacksScored[_gameId][_player][0];
+            uint256 totalLowKick = directAttacksScored[_gameId][_player][1];
+            uint256 totalLowThunder = directAttacksScored[_gameId][_player][2];
+            uint256 totalHardPunch = directAttacksScored[_gameId][_player][3];
+            uint256 totalHardKick = directAttacksScored[_gameId][_player][4];
+            uint256 totalHardThunder = directAttacksScored[_gameId][_player][5];
+            uint256 totalSlash = directAttacksScored[_gameId][_player][6];
             directAttacks[0] = totalLowPunch;
             directAttacks[1] = totalLowKick;
             directAttacks[2] = totalLowThunder;
@@ -112,20 +144,28 @@ contract Betting is Proxied, Guard {
           }
 
       // get the total number of blocked attacks of each hitType of the given corner in a game
-      function getBlockedAttacksScored(uint256 _gameId, address _supportedPlayer)
+      /**
+       * @author @ziweidream
+       * @notice get the total number of blocked attacks of each hitType of the given corner in a game
+       * @param _gameId the gameID of the game
+       * @param _player the given corner in this game for whom the total number of the blocked attacks are obtained
+       * @return an array of fixed size 7 blockedAttacks, which contains the total number of blocked attacks of each hitType
+       * of the given corner in this game
+       */
+      function getBlockedAttacksScored(uint256 _gameId, address _player)
           public
           view
           returns(
               uint256[7] memory blockedAttacks
               )
           {
-              uint256 totalLowPunch = blockedAttacksScored[_gameId][_supportedPlayer][0];
-              uint256 totalLowKick = blockedAttacksScored[_gameId][_supportedPlayer][1];
-              uint256 totalLowThunder = blockedAttacksScored[_gameId][_supportedPlayer][2];
-              uint256 totalHardPunch = blockedAttacksScored[_gameId][_supportedPlayer][3];
-              uint256 totalHardKick = blockedAttacksScored[_gameId][_supportedPlayer][4];
-              uint256 totalHardThunder = blockedAttacksScored[_gameId][_supportedPlayer][5];
-              uint256 totalSlash = blockedAttacksScored[_gameId][_supportedPlayer][6];
+              uint256 totalLowPunch = blockedAttacksScored[_gameId][_player][0];
+              uint256 totalLowKick = blockedAttacksScored[_gameId][_player][1];
+              uint256 totalLowThunder = blockedAttacksScored[_gameId][_player][2];
+              uint256 totalHardPunch = blockedAttacksScored[_gameId][_player][3];
+              uint256 totalHardKick = blockedAttacksScored[_gameId][_player][4];
+              uint256 totalHardThunder = blockedAttacksScored[_gameId][_player][5];
+              uint256 totalSlash = blockedAttacksScored[_gameId][_player][6];
               blockedAttacks[0] = totalLowPunch;
               blockedAttacks[1] = totalLowKick;
               blockedAttacks[2] = totalLowThunder;
@@ -136,36 +176,76 @@ contract Betting is Proxied, Guard {
           }
 
     // fillBets() is internal. Temporarily set as public for truffle test purpose.
-    // record the bet amount of each individual bet of the given corner of a game with a specific gameId
-    function fillBets(uint256 _gameId, address _supportedPlayer, uint256 _betAmount) public {
-      bets[_gameId][_supportedPlayer].push(_betAmount);
+   /**
+    * @author @ziweidream
+    * @notice record the bet ether amount of each individual bet placed by the given corner
+    * in a game with a specific gameId
+    * @param _gameId the gameID of the game
+    * @param _player the given corner in this game for whom the individual bet ether amount is recorded
+    * @param _betAmount the ether amount of the bet placed by the given corner
+    */
+    function fillBets(uint256 _gameId, address _player, uint256 _betAmount) public {
+      bets[_gameId][_player].push(_betAmount);
     }
 
     // get last 5 bet amount of the given corner
-    function getLastFiveBets(uint256 _gameId, address _supportedPlayer)
+   /**
+    * @author @ziweidream
+    * @notice get the ether amount of the last 5 bets placed by the given corner in a game
+    * with a specific gameId
+    * @dev lastBet1 is the last bet, lastBet2 is the second last bet, and so forth
+    * @param _gameId the gameID of the game
+    * @param _player the given corner in this game whose individual bet ether amount is recorded
+    * @return the last 5 bet ether amount by the given corner in a game
+    */
+    function getLastFiveBets(uint256 _gameId, address _player)
         public
         view
         returns(uint lastBet5, uint lastBet4, uint lastBet3, uint lastBet2, uint lastBet1)
     {
-      uint256 arrLength = bets[_gameId][_supportedPlayer].length;
-      lastBet5 = bets[_gameId][_supportedPlayer][arrLength.sub(5)];
-      lastBet4 = bets[_gameId][_supportedPlayer][arrLength.sub(4)];
-      lastBet3 = bets[_gameId][_supportedPlayer][arrLength.sub(3)];
-      lastBet2 = bets[_gameId][_supportedPlayer][arrLength.sub(2)];
-      lastBet1 = bets[_gameId][_supportedPlayer][arrLength.sub(1)];
+      uint256 arrLength = bets[_gameId][_player].length;
+      lastBet5 = bets[_gameId][_player][arrLength.sub(5)];
+      lastBet4 = bets[_gameId][_player][arrLength.sub(4)];
+      lastBet3 = bets[_gameId][_player][arrLength.sub(3)];
+      lastBet2 = bets[_gameId][_player][arrLength.sub(2)];
+      lastBet1 = bets[_gameId][_player][arrLength.sub(1)];
   }
 
     // setLastBetTimestamp() is internal. Temporarily set as public for truffle test purpose.
-    function setLastBetTimestamp(uint256 _gameId, address _supportedPlayer, uint256 _lastBetTimestamp) public {
-        lastBetTimestamp[_gameId][_supportedPlayer] = _lastBetTimestamp;
+    /**
+    * @author @ziweidream
+    * @notice record the last bet timestamp for the given corner in a game with a specific gameId
+    * @param _gameId the gameID of the game
+    * @param _player the given corner in this game whose last bet timestamp is recorded
+    * @param _lastBetTimestamp the timestamp of the last bet placed by the given corner in the game
+    */
+    function setLastBetTimestamp(uint256 _gameId, address _player, uint256 _lastBetTimestamp) public {
+        lastBetTimestamp[_gameId][_player] = _lastBetTimestamp;
     }
 
     // setDefenseLevel() is internal. Temporarily set as public for truffle test purpose.
+   /**
+    * @author @ziweidream
+    * @notice record the current defense level of the given corner in a game with a specific gameId
+    * @param _gameId the gameID of the game
+    * @param _player the given corner in this game for whom the defense level is recorded
+    * @param _defenseLevel the current defense level of the given corner
+    */
     function setDefenseLevel(uint256 _gameId, address _player, uint256 _defenseLevel) public {
         defenseLevel[_gameId][_player] = _defenseLevel;
     }
 
-    // randomly select attack types from low values column or high values column depending on the bet ether amount
+   /**
+    * @author @ziweidream
+    * @notice randomly select attack types from low values column or high values column depending on
+    * the bet ether amount by the given corner in a game with a specific gameId
+    * @param _gameId the gameID of the game
+    * @param _supportedPlayer the given corner in this game for whom the attack type is randomly selected
+    * @param _lastBetAmount the last bet ether amount placed by the supported player in the game
+    * @param _randomNum the random number generated by the front end when Button bet is pressed by the given corner
+    * @return the attackType randomly selected for the given corner, the attackHash for the attackType selected,
+    * and the index of the attackHash returned in the fight map
+    */
     function getAttackType(
         uint256 _gameId,
         address _supportedPlayer,
@@ -182,7 +262,7 @@ contract Betting is Proxied, Guard {
         if (_lastBetAmount <= prevBetAmount) {
             uint256 diceLowValues = randomGen(_randomNum);
             if (diceLowValues <= 33) {
-                attackType = fightMap[_gameId][0].attack;//attacksColumn[0];
+                attackType = fightMap[_gameId][0].attack;
                 attackHash = fightMap[_gameId][0].hash;
                 index = 0;
             } else if (diceLowValues <= 66 && diceLowValues > 33) {
@@ -221,9 +301,17 @@ contract Betting is Proxied, Guard {
     // determine whether the attack type is blocked or direct
     // if opponent's defence level = 0, all attacks are direct
     // if opponent has been inactive for more than 5 seconds since last bet, attack is direct. Otherwise the attack is blocked.
+   /**
+    * @author @ziweidream
+    * @notice determine whether the attack type by the supported player is blocked or direct in a game with a specific gameId
+    * @param _gameId the gameID of the game
+    * @param _opponentPlayer the address of the opponent player in this game
+    * @return true if the attack type is blocked, false if it is direct
+    */
     function isAttackBlocked(uint256 _gameId, address _opponentPlayer) public view returns(bool) {
-        // get the last bet timestamp of the given corner
         uint256 _lastBetTimestamp = lastBetTimestamp[_gameId][_opponentPlayer];
+        // If opponent has been inactive for more than 5 seconds since last bet, attack is direct.
+        // Otherwise it is blocked.
         if(_lastBetTimestamp >= now.sub(5)) {
             return true;
         }
@@ -234,6 +322,17 @@ contract Betting is Proxied, Guard {
      // a bet is received from attacker and the last five bets are compared on the condition to see 
      // if each bet was bigger than the previous bet in progression.
      // reduceDefenseLevel() is internal. Temporarily set as public for truffle test purpose.
+   /**
+    * @author @ziweidream
+    * @notice reduce the defense level of the opponent if each of the last 5 bets ether amount from
+    * the attacker was consecutively bigger than the previous one in a game with a specific gameId
+    * @param _gameId the gameID of the game
+    * @param _supportedPlayer the address of the supported player in this game
+    * whose last 5 bets ether amount are compared
+    * @param _opponentPlayer the address of the opponent player in this game
+    * whose defense level is reduced if contidions stated above are met
+    * @return the defense level of the opponent player in the game
+    */
      function reduceDefenseLevel(
         uint256 _gameId,
         address _supportedPlayer,
@@ -264,6 +363,14 @@ contract Betting is Proxied, Guard {
     }
 
      // temporarily comment out onlyContract(CONTRACT_NAME_GAMEMANAGER) until GameManager.sol is furhter defined/developed
+   /**
+    * @author @ziweidream
+    * @notice generates a fight map for a game with a specific gameId
+    * @dev this function can only be called by the GameManager contract
+    * @param _gameId the gameID of the game
+    * @param _randomRed the random number generated when the Red corner presses the Button Bet
+    * @param _randomBlack the random number generated when the Black corner presses the Button Bet
+    */
     function startGame(
         uint256 _gameId,
         uint256 _randomRed,
@@ -276,6 +383,20 @@ contract Betting is Proxied, Guard {
     }
 
     // temporarily comment out onlyContract(CONTRACT_NAME_GAMEMANAGER) until GameManager.sol is furhter defined/developed
+   /**
+    * @author @ziweidream
+    * @notice determines the attack type, attackHash, and current opponent defense level partially
+    * depending on the effect of each bet placed by the given corner in a game with a specific gameId
+    * @dev this function can only be called by the GameManager contract
+    * @param _gameId the gameID of the game
+    * @param _lastBetAmount the last bet ether amount placed by the supported player in the game
+    * @param _supportedPlayer the address of the supported player in the game
+    * @param _opponentPlayer the address of the opponent player in the game
+    * @param _randomNum the random number generated when the supported player presses Button Bet
+    * in the game
+    * @return the attack type and attackHash for the supported player,
+    * and current defense level of the opponent player
+    */
     function bet(
         uint256 _gameId,
         uint256 _lastBetAmount,
