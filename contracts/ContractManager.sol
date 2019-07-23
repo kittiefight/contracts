a6c69d4932ad37b60d561b889d5a0749fa0b0b4b
Pavel Rubin <pash7ka@gmail.com>
2019-07-23 17:35:05 +0300

    Add error message in case of contract address not set

--- contracts/ContractManager.sol
+++ contracts/ContractManager.sol
@@ -25,7 +25,14 @@ contract ContractManager is Owned, IContractManager {
     }
 
     function getContract(string memory name) public view returns (address) {
-        require(contracts[name] != address(0));
+        require(contracts[name] != address(0), 'Contract not registered');
+        // if(contracts[name] == address(0)) {
+        //     bytes memory error = bytes(abi.encodePacked('Contract not registered: ', name));
+        //     //revert('Contract not registered');            
+        //     assembly {
+        //         revert(add(name,32), name)
+        //     }
+        // }
 
         return contracts[name];
     }
