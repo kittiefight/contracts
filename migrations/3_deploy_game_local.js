e04f97210a2be0fd4d63585cb1593bfede8dea94
Zi Wei Zhang <ziweidream@gmail.com>
2020-08-13 00:27:19 -0400

    Update 2_deploy_game.js (WIP)

--- migrations/3_deploy_game_local.js
+++ migrations/3_deploy_game_local.js
@@ -80,8 +80,6 @@ const INITIAL_ETH_ENDOWMENT = new BigNumber(
 
 // ================ GAME VARS AND FEES ================ //
 const LISTING_FEE = new BigNumber(web3.utils.toWei("125", "ether"));
-//const TICKET_FEE = new BigNumber(web3.utils.toWei("37.5", "ether"));
-//const BETTING_FEE = new BigNumber(web3.utils.toWei("2.5", "ether"));
 const MIN_CONTRIBUTORS = 2
 const REQ_NUM_MATCHES = 10
 const GAME_PRESTART = 30 // 2 min
