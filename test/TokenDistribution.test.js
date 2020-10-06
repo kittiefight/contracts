baaa45f1f90966af6d912063df3027552e740c7a
Zi Wei Zhang <ziweidream@gmail.com>
2020-10-06 16:30:33 -0400

    add more tests in TokenDistribution.test.js

--- test/TokenDistribution.test.js
+++ test/TokenDistribution.test.js
@@ -78,7 +78,9 @@ contract("TokenDistribution", accounts => {
   });
 
   it("initialize tokenDistribution contract", async () => {
-    let investors = [], ethAmounts = [], eth_amount;
+    let investors = [],
+      ethAmounts = [],
+      eth_amount;
     let standardRate = new BigNumber(
       web3.utils.toWei("1000", "ether") // 1000
     );
@@ -95,20 +97,20 @@ contract("TokenDistribution", accounts => {
     }
 
     for (let i = 1; i < 9; i++) {
-        eth_amount = new BigNumber(
-          web3.utils.toWei(randomValue(10).toString(), "ether")
-        );
-        investors.push(accounts[i]);
-        ethAmounts.push(eth_amount);
-      }
+      eth_amount = new BigNumber(
+        web3.utils.toWei(randomValue(10).toString(), "ether")
+      );
+      investors.push(accounts[i]);
+      ethAmounts.push(eth_amount);
+    }
 
-      for (let i = 8; i < 19; i++) {
-        eth_amount = new BigNumber(
-          web3.utils.toWei(randomValue(15).toString(), "ether")
-        );
-        investors.push(accounts[i]);
-        ethAmounts.push(eth_amount);
-      }
+    for (let i = 8; i < 19; i++) {
+      eth_amount = new BigNumber(
+        web3.utils.toWei(randomValue(15).toString(), "ether")
+      );
+      investors.push(accounts[i]);
+      ethAmounts.push(eth_amount);
+    }
 
     await tokenDistribution.initialize(
       investors,
@@ -120,107 +122,78 @@ contract("TokenDistribution", accounts => {
     ).should.be.fulfilled;
 
     let token_bonus = new BigNumber(
-        web3.utils.toWei("5000000", "ether") // 5 million
-      );
+      web3.utils.toWei("5000000", "ether") // 5 million
+    );
 
-    await kittieFightToken.transfer(tokenDistribution.address, token_bonus)
+    await kittieFightToken.transfer(tokenDistribution.address, token_bonus);
   });
 
   it("imports investmet lists", async () => {
-      let investmentInfo;
-      for (let i = 1; i < 19; i++) {
-          investmentInfo = await tokenDistribution.getInvestment(i)
-          console.log("Investment ID:", i)
-          console.log("Investor:", investmentInfo[0])
-          console.log("Ether invested:", weiToEther(investmentInfo[1]))
-          console.log("Has bonus been claimed from this investment?", investmentInfo[2])
-      }
-  })
+    let investmentInfo;
+    for (let i = 1; i < 19; i++) {
+      investmentInfo = await tokenDistribution.getInvestment(i);
+      console.log("Investment ID:", i);
+      console.log("Investor:", investmentInfo[0]);
+      console.log("Ether invested:", weiToEther(investmentInfo[1]));
+      console.log(
+        "Has bonus been claimed from this investment?",
+        investmentInfo[2]
+      );
+    }
+  });
 
   it("sets standardRate, percentage bonus and withdraw date", async () => {
-      let standardRate = await tokenDistribution.standardRate.call()
-      console.log("Standard Rate:", weiToEther(standardRate))
-      let percentBonus = await tokenDistribution.percentBonus.call()
-      console.log("Percentage Bonus:", weiToEther(percentBonus))
-      let withdrawDate = await tokenDistribution.withdrawDate.call()
-      console.log("Withdraw date:", withdrawDate.toString())
-  })
+    let standardRate = await tokenDistribution.standardRate.call();
+    console.log("Standard Rate:", weiToEther(standardRate));
+    let percentBonus = await tokenDistribution.percentBonus.call();
+    console.log("Percentage Bonus:", weiToEther(percentBonus));
+    let withdrawDate = await tokenDistribution.withdrawDate.call();
+    console.log("Withdraw date:", withdrawDate.toString());
+  });
 
   it("an investor cannot withdraw before withdraw date", async () => {
-      let canWithdraw = await tokenDistribution.canWithdraw()
-      console.log("Can withdraw?", canWithdraw[0])
-      console.log("Time until withdraw:", canWithdraw[1].toString())
-      for (let i=1; i<11; i++) {
-        await tokenDistribution.withdraw({from: accounts[i]}).should.be.rejected;
-      } 
-  })
+    let canWithdraw = await tokenDistribution.canWithdraw();
+    console.log("Can withdraw?", canWithdraw[0]);
+    console.log("Time until withdraw:", canWithdraw[1].toString());
+    for (let i = 1; i < 11; i++) {
+      await tokenDistribution.withdraw({from: accounts[i]}).should.be.rejected;
+    }
+  });
 
   it("an investor can withdraw after withdraw date", async () => {
-    let canWithdraw = await tokenDistribution.canWithdraw()
-    console.log("Can withdraw?", canWithdraw[0])
-    let timeTillWithdraw = Number(canWithdraw[1].toString())
-    console.log("Time until withdraw:", timeTillWithdraw)
+    let canWithdraw = await tokenDistribution.canWithdraw();
+    console.log("Can withdraw?", canWithdraw[0]);
+    let timeTillWithdraw = Number(canWithdraw[1].toString());
+    console.log("Time until withdraw:", timeTillWithdraw);
     if (timeTillWithdraw > 0) {
-        await advanceTimeAndBlock(timeTillWithdraw+180);
-        console.log("Time flies...")
+      await advanceTimeAndBlock(timeTillWithdraw + 180);
+      console.log("Time flies...");
     }
-    canWithdraw = await tokenDistribution.canWithdraw()
-    console.log("Can withdraw?", canWithdraw[0])
-  
-      let investor = accounts[1], investmentIDs, ethAmount, bonus
-      // get all investments belonging to this investor
-      investmentIDs = await tokenDistribution.getInvestmentIDs(investor);
-      for (let i=0; i<investmentIDs.length; i++) {
-          console.log("Investment ID:", investmentIDs[i].toString())
-          ethAmount = await tokenDistribution.getInvestment(Number(investmentIDs[i].toString()))
-          console.log("Ether invested:", weiToEther(ethAmount[1]))
-          eth_amount = new BigNumber(
-            web3.utils.toWei(weiToEther(ethAmount[1]), "ether")
-          );
-          bonus = await tokenDistribution.calculatePrincipalAndBonus(eth_amount)
-          console.log("Principal calculated:", weiToEther(bonus[0]))
-          console.log("Token Bonus calculated:", weiToEther(bonus[1]))
-          console.log("Total rewards calculated:", weiToEther(bonus[2]))
-          await tokenDistribution.withdraw(Number(investmentIDs[i].toString()), { from: investor }).should.be.fulfilled;
-      }
+    canWithdraw = await tokenDistribution.canWithdraw();
+    console.log("Can withdraw?", canWithdraw[0]);
 
-      let newWithdraw = await tokenDistribution.getPastEvents("WithDrawn", {
-        fromBlock: 0,
-        toBlock: "latest"
-      });
-  
-      newWithdraw.map(async (e) => {
-        console.log('\n==== NEW WITHDRAW HAPPENED ===');
-        console.log('    Investor ', e.returnValues.investor)
-        console.log('    InvestmentID ', e.returnValues.investmentID)
-        console.log('    Principal ', weiToEther(e.returnValues.principal))
-        console.log('    Bonus ', weiToEther(e.returnValues.bonus))
-        console.log('    WithdrawTime ', e.returnValues.withdrawTime)
-        console.log('========================\n')
-      })
-  })
-
-  it ("many investors withdraw", async () => {
-    let investor, investmentIDs, ethAmount, bonus
-    for (let m = 2; m < 19; m++) {
-      investor = accounts[m]
-      console.log("Investor: user", m)
-      // get all investments belonging to this investor
-      investmentIDs = await tokenDistribution.getInvestmentIDs(investor);
-      for (let i=0; i<investmentIDs.length; i++) {
-          console.log("Investment ID:", investmentIDs[i].toString())
-          ethAmount = await tokenDistribution.getInvestment(Number(investmentIDs[i].toString()))
-          console.log("Ether invested:", weiToEther(ethAmount[1]))
-          eth_amount = new BigNumber(
-            web3.utils.toWei(weiToEther(ethAmount[1]), "ether")
-          );
-          bonus = await tokenDistribution.calculatePrincipalAndBonus(eth_amount)
-          console.log("Principal calculated:", weiToEther(bonus[0]))
-          console.log("Token Bonus calculated:", weiToEther(bonus[1]))
-          console.log("Total rewards calculated:", weiToEther(bonus[2]))
-          await tokenDistribution.withdraw(Number(investmentIDs[i].toString()), { from: investor }).should.be.fulfilled;
-      }
-
+    let investor = accounts[1],
+      investmentIDs,
+      ethAmount,
+      bonus;
+    // get all investments belonging to this investor
+    investmentIDs = await tokenDistribution.getInvestmentIDs(investor);
+    for (let i = 0; i < investmentIDs.length; i++) {
+      console.log("Investment ID:", investmentIDs[i].toString());
+      ethAmount = await tokenDistribution.getInvestment(
+        Number(investmentIDs[i].toString())
+      );
+      console.log("Ether invested:", weiToEther(ethAmount[1]));
+      eth_amount = new BigNumber(
+        web3.utils.toWei(weiToEther(ethAmount[1]), "ether")
+      );
+      bonus = await tokenDistribution.calculatePrincipalAndBonus(eth_amount);
+      console.log("Principal calculated:", weiToEther(bonus[0]));
+      console.log("Token Bonus calculated:", weiToEther(bonus[1]));
+      console.log("Total rewards calculated:", weiToEther(bonus[2]));
+      await tokenDistribution.withdraw(Number(investmentIDs[i].toString()), {
+        from: investor
+      }).should.be.fulfilled;
     }
 
     let newWithdraw = await tokenDistribution.getPastEvents("WithDrawn", {
@@ -228,30 +201,99 @@ contract("TokenDistribution", accounts => {
       toBlock: "latest"
     });
 
-    newWithdraw.map(async (e) => {
-      console.log('\n==== NEW WITHDRAW HAPPENED ===');
-      console.log('    Investor ', e.returnValues.investor)
-      console.log('    InvestmentID ', e.returnValues.investmentID)
-      console.log('    Principal ', weiToEther(e.returnValues.principal))
-      console.log('    Bonus ', weiToEther(e.returnValues.bonus))
-      console.log('    WithdrawTime ', e.returnValues.withdrawTime)
-      console.log('========================\n')
-    })
-  })
+    newWithdraw.map(async e => {
+      console.log("\n==== NEW WITHDRAW HAPPENED ===");
+      console.log("    Investor ", e.returnValues.investor);
+      console.log("    InvestmentID ", e.returnValues.investmentID);
+      console.log("    Principal ", weiToEther(e.returnValues.principal));
+      console.log("    Bonus ", weiToEther(e.returnValues.bonus));
+      console.log("    WithdrawTime ", e.returnValues.withdrawTime);
+      console.log("========================\n");
+    });
+  });
+
+  it("an investment that has been claimed rewards cannot be claimed again", async () => {
+    let investor = accounts[1],
+      investmentIDs;
+    // get all investments belonging to this investor
+    investmentIDs = await tokenDistribution.getInvestmentIDs(investor);
+    for (let i = 0; i < investmentIDs.length; i++) {
+      await tokenDistribution.withdraw(Number(investmentIDs[i].toString()), {
+        from: investor
+      }).should.be.rejected;
+    }
+  });
+
+  it("an investor cannot claim from an investment that is not made by him/her", async () => {
+    let investor = accounts[2],
+      investmentIDs;
+    // get all investments belonging to this investor
+    investmentIDs = await tokenDistribution.getInvestmentIDs(investor);
+    for (let i = 0; i < investmentIDs.length; i++) {
+      await tokenDistribution.withdraw(Number(investmentIDs[i].toString()), {
+        from: accounts[3]
+      }).should.be.rejected;
+    }
+  });
+
+  it("many investors withdraw", async () => {
+    let investor, investmentIDs, ethAmount, bonus;
+    for (let m = 2; m < 19; m++) {
+      investor = accounts[m];
+      console.log("Investor: user", m);
+      // get all investments belonging to this investor
+      investmentIDs = await tokenDistribution.getInvestmentIDs(investor);
+      for (let i = 0; i < investmentIDs.length; i++) {
+        console.log("Investment ID:", investmentIDs[i].toString());
+        ethAmount = await tokenDistribution.getInvestment(
+          Number(investmentIDs[i].toString())
+        );
+        console.log("Ether invested:", weiToEther(ethAmount[1]));
+        eth_amount = new BigNumber(
+          web3.utils.toWei(weiToEther(ethAmount[1]), "ether")
+        );
+        bonus = await tokenDistribution.calculatePrincipalAndBonus(eth_amount);
+        console.log("Principal calculated:", weiToEther(bonus[0]));
+        console.log("Token Bonus calculated:", weiToEther(bonus[1]));
+        console.log("Total rewards calculated:", weiToEther(bonus[2]));
+        await tokenDistribution.withdraw(Number(investmentIDs[i].toString()), {
+          from: investor
+        }).should.be.fulfilled;
+      }
+    }
+
+    let newWithdraw = await tokenDistribution.getPastEvents("WithDrawn", {
+      fromBlock: 0,
+      toBlock: "latest"
+    });
+
+    newWithdraw.map(async e => {
+      console.log("\n==== NEW WITHDRAW HAPPENED ===");
+      console.log("    Investor ", e.returnValues.investor);
+      console.log("    InvestmentID ", e.returnValues.investmentID);
+      console.log("    Principal ", weiToEther(e.returnValues.principal));
+      console.log("    Bonus ", weiToEther(e.returnValues.bonus));
+      console.log("    WithdrawTime ", e.returnValues.withdrawTime);
+      console.log("========================\n");
+    });
+  });
 
   it("updataes investmet info", async () => {
     let investmentInfo;
     for (let i = 1; i < 19; i++) {
-        investmentInfo = await tokenDistribution.getInvestment(i)
-        console.log("Investment ID:", i)
-        console.log("Investor:", investmentInfo[0])
-        console.log("Ether invested:", weiToEther(investmentInfo[1]))
-        console.log("Has bonus been claimed from this investment?", investmentInfo[2])
-        console.log("Principal Claimed:", weiToEther(investmentInfo[3]))
-        console.log("Bonus Claimed:", weiToEther(investmentInfo[4]))
-        console.log("Claim Time:", weiToEther(investmentInfo[5]))
+      investmentInfo = await tokenDistribution.getInvestment(i);
+      console.log("Investment ID:", i);
+      console.log("Investor:", investmentInfo[0]);
+      console.log("Ether invested:", weiToEther(investmentInfo[1]));
+      console.log(
+        "Has bonus been claimed from this investment?",
+        investmentInfo[2]
+      );
+      console.log("Principal Claimed:", weiToEther(investmentInfo[3]));
+      console.log("Bonus Claimed:", weiToEther(investmentInfo[4]));
+      console.log("Claim Time:", investmentInfo[5].toString());
     }
-})
+  });
 
   it("transfers leftover rewards to a new address", async () => {
     let advancement = 10 * 24 * 60 * 60;
@@ -262,6 +304,16 @@ contract("TokenDistribution", accounts => {
       "Final KTY balance in YieldFarming contract:",
       weiToEther(KTY_bal)
     );
-   
+
+    if (Number(weiToEther(KTY_bal)) > 0) {
+      let KTY_leftover = new BigNumber(
+        web3.utils.toWei(weiToEther(KTY_bal), "ether")
+      );
+      await tokenDistribution.returnTokens(
+        kittieFightToken.address,
+        KTY_leftover,
+        accounts[0]
+      ).should.be.fulfilled;
+    }
   });
 });
