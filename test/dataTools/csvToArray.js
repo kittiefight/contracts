dac3567893fdd82cc192411abe13b45925b45aa9
Zi Wei Zhang <ziweidream@gmail.com>
2020-10-12 18:03:07 -0400

    excluding from address equal to private sale address

--- test/dataTools/csvToArray.js
+++ test/dataTools/csvToArray.js
@@ -4,7 +4,7 @@ const csv = require("fast-csv");
 
 const BigNumber = require('bignumber.js')
 
-// node test/dataTools/csvToArray.js "investments2.csv" ''
+// node test/dataTools/csvToArray.js "investments2.csv" privateSaleAddress ''
 
 // if want to exclude an address, put the address inside ''
 // if there are more than 1 address to exclude, list all addresses inside '' separated by comma,
@@ -12,7 +12,8 @@ const BigNumber = require('bignumber.js')
 // node test/dataTools/csvToArray.js "investments2.csv" '0x258ac32ad0b0806c9ecc9d2cc58dd7f66314b2b5,0xfd0d518b68f0d40ef6f14f68e7298b0418a09c2a'
 
 let file = process.argv[2]
-let removeAddrs = process.argv[3].split(',')
+let privateSaleAddr = process.argv[3]
+let removeAddrs = process.argv[4].split(',')
 
 console.log("====================== EtherScan Investment File ======================")
 console.log('\n======================  addresses ======================\n')
@@ -21,6 +22,9 @@ let addressArray = [];
 let ethArray = []
 
 function addInvestment(addr, eth) {
+  if ( addr.toLowerCase() == privateSaleAddr.toLowerCase() ) {
+    return;
+  }
     if (eth == 0) {
       return;
     }
